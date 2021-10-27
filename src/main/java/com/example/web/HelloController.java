fb1a39863b0ad406f4fe1901b554ac8926173221
Diego Araujo <diegogomesaraujo@gmail.com>
2021-10-27 20:27:17 -0300

    fisrt commit

---
+++ src/main/java/com/example/web/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @Value("${build.version}")
+    private int buildVersion;
+
+    @GetMapping
+    public String hello() {
+        return "Build "+ buildVersion;
+    }
+
+}
