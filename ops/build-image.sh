4d0c8529adc40deea927f9ff3730207b8493eb84
Diego Araujo <diegogomesaraujo@gmail.com>
2021-10-27 22:12:24 -0300

    dockerfile

--- ops/build-image.sh
+++ ops/build-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -f ops/Dockerfile diegogomesaraujo/hello-pipeline:$GO_PIPELINE_COUNTER
+docker build -f ops/Dockerfile -t diegogomesaraujo/hello-pipeline:$GO_PIPELINE_COUNTER
 #docker push diegogomesaraujo/hello-pipeline:$GO_PIPELINE_COUNTER
